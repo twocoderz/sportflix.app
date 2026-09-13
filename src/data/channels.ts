00b8c74cf54a19d2a9122303ee818914bfb0b20c
Rahim10020 <rahialighi@gmail.com>
2026-09-13 19:27:26 +0000

    update logos

--- src/data/channels.ts
+++ src/data/channels.ts
@@ -5,35 +5,72 @@ export type Channel = {
 };
 
 export const channels: Channel[] = [
-  { id: "daria", name: "Daria", logo: "/tv_logos/01_Daria.png" },
-  { id: "duck-tv", name: "Duck TV", logo: "/tv_logos/02_Duck TV.png" },
   {
-    id: "filmrise-anime",
-    name: "FilmRise Anime",
-    logo: "/tv_logos/03_FilmRise Anime.png",
-  },
-  { id: "gong", name: "Gong", logo: "/tv_logos/04_Gong.png" },
-  {
-    id: "mr-bean-anime",
-    name: "Mr Bean Animé",
-    logo: "/tv_logos/05_Mr Bean Animé.png",
-  },
-  { id: "naruto", name: "Naruto", logo: "/tv_logos/06_Naruto.png" },
-  {
-    id: "pluto-tv-retro-toons",
-    name: "Pluto TV Retro Toons",
-    logo: "/tv_logos/07_Pluto TV Retro Toons.png",
+    id: "africa-24-sport",
+    name: "Africa 24 Sport",
+    logo: "/tv_logos/01_Africa 24 Sport.png",
   },
   {
-    id: "pluto-tv-toons",
-    name: "Pluto TV Toons",
-    logo: "/tv_logos/08_Pluto TV Toons.png",
+    id: "bein-sports-xtra",
+    name: "beIN SPORTS XTRA",
+    logo: "/tv_logos/02_beIN SPORTS XTRA.png",
   },
   {
-    id: "retrocrush",
-    name: "RetroCrush",
-    logo: "/tv_logos/09_RetroCrush.png",
+    id: "canal-sport-360",
+    name: "CANAL+ SPORT 360",
+    logo: "/tv_logos/03_CANAL+ SPORT 360.png",
   },
-  { id: "tiny-pop", name: "Tiny Pop", logo: "/tv_logos/10_Tiny Pop.png" },
-  { id: "yu-gi-oh", name: "Yu-Gi-Oh!", logo: "/tv_logos/11_Yu-Gi-Oh.png" },
+  { id: "equidia", name: "Equidia", logo: "/tv_logos/04_Equidia.png" },
+  {
+    id: "eurosport-1",
+    name: "Eurosport 1",
+    logo: "/tv_logos/05_Eurosport 1.svg",
+  },
+  {
+    id: "eurosport-2",
+    name: "Eurosport 2",
+    logo: "/tv_logos/06_Eurosport 2.svg",
+  },
+  {
+    id: "fifa-plus-french",
+    name: "FIFA+ French",
+    logo: "/tv_logos/07_FIFA+ French.png",
+  },
+  { id: "foot-plus", name: "Foot+", logo: "/tv_logos/08_Foot+.png" },
+  {
+    id: "golf-channel",
+    name: "Golf Channel",
+    logo: "/tv_logos/09_Golf Channel.svg",
+  },
+  {
+    id: "kozoom-tv",
+    name: "Kozoom TV",
+    logo: "/tv_logos/10_Kozoom TV.png",
+  },
+  { id: "lequipe", name: "L'Équipe", logo: "/tv_logos/11_L'Équipe.png" },
+  {
+    id: "lequipe-live-foot",
+    name: "L'Équipe Live Foot",
+    logo: "/tv_logos/12_L'Équipe Live Foot.png",
+  },
+  {
+    id: "rmc-sport-1",
+    name: "RMC Sport 1",
+    logo: "/tv_logos/13_RMC Sport 1.png",
+  },
+  {
+    id: "sport-en-france",
+    name: "Sport en France",
+    logo: "/tv_logos/14_Sport en France.png",
+  },
+  {
+    id: "tennis-channel",
+    name: "Tennis Channel",
+    logo: "/tv_logos/15_Tennis Channel.svg",
+  },
+  {
+    id: "trace-sport-stars",
+    name: "Trace Sport Stars",
+    logo: "/tv_logos/16_Trace Sport Stars.png",
+  },
 ];
